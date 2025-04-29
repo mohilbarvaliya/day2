4a8abd44746d26bb47207289e8d655aec87e0a19
mohilbarvaliya <mohilbarvaliya1771@gmail.com>
2025-04-30 02:36:57 +0530

    fetch api cmd

--- lib/firebase_options.dart
+++ lib/firebase_options.dart
@@ -22,3 +22,13 @@ class DefaultFirebaseOptions {
 // git branch -M main
 // git push -u origin main
 
+//  Future<User> fetchUser() async {
+//     final response =
+//         await http.get(Uri.parse('https://jsonplaceholder.typicode.com/users/1'));
+
+//     if (response.statusCode == 200) {
+//       return User.fromJson(jsonDecode(response.body));
+//     } else {
+//       throw Exception('Failed to load user');
+//     }
+//   }
