211d25d9556d3b28d3e499a69870295c4f1f8434
mohilbarvaliya <mohilbarvaliya1771@gmail.com>
2025-04-30 11:24:15 +0530

    add some feture

--- lib/firebase_options.dart
+++ lib/firebase_options.dart
@@ -3,12 +3,12 @@ import 'package:firebase_core/firebase_core.dart';
 class DefaultFirebaseOptions {
   static FirebaseOptions get currentPlatform {
     return FirebaseOptions(
-      apiKey: "AIzaSyDkD1VNw3v5je0dB5NKEtSJZANYLZHegKk",
+      apiKey: "SyDkD1VNw3v5je0dB5NKEtSJZANYLZHegKk",
   authDomain: "trial-76934.firebaseapp.com",
   projectId: "trial-76934",
   storageBucket: "trial-76934.firebasestorage.app",
   messagingSenderId: "833452833324",
-  appId: "1:833452833324:web:98d3eade81e9563ef86f89",
+  appId: "1:833452833324:web:98d3eade81e9563ef89",
   measurementId: "G-ZNM3JDW591"
     );
   }
@@ -16,7 +16,7 @@ class DefaultFirebaseOptions {
 
 // git init
 // git add .
-// git commit -m "Initial commit"
+// git commit -m "Initial commit:AIza  f86"
 
 // git remote add origin https://github.com/yourusername/my_flutter_app.git
 // git branch -M main
